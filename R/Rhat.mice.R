347d53b9f2b6611a1ed559d5c2d46a2bce09416b
Alexander Robitzsch <a.robitzsch@bifie.at>
2014-05-08 12:00:20 +0000

    version 0.12-9

--- R/Rhat.mice.R
+++ R/Rhat.mice.R
@@ -1,5 +1,27 @@
 Rhat.mice <-
 function( mice.object){ 
+        #############################################################################
+#	if (class( mice.object) == "mids.1chain"){	
+#		mice.object <- mice.object$midsobj		
+#		}
+    chainMean <- mice.object$chainMean
+    chainVar <- mice.object$chainVar
+    dcM <- dim(chainMean)
+    dfr <- data.frame(matrix( 0 , nrow= dcM[1] , ncol= 4 ))
+    for (vv in 1:dcM[1] ){ 
+        # vv <- 1
+        dfr[vv,3] <- Rhat( chainMean[vv,,] )
+        dfr[vv,4] <- Rhat( chainVar[vv,,] )
+        dfr[vv,1] <- rownames(chainMean[,,1])[vv]
+        dfr[vv,2] <- 100*mean( is.na( mice.object$data[ , dfr[vv,1] ] ) )
+                }
+    colnames(dfr) <- c("variable" , "MissProp" , "Rhat.M.imp" , "Rhat.Var.imp" )
+    print(dfr )
+    invisible(dfr)
+        }
+
+####################################
+# auxiliary functions for Rhat statistic				
         ############################################################################
         # Code from rube package
         # Source: http://www.stat.cmu.edu/~hseltman/rube/rube0.2-16/R/Rhat.R
@@ -32,23 +54,4 @@ function( mice.object){
         if (dm[2]==1) return(NULL)
         if (dm[3]==1) return(Rhat1(arr[,,1]))
         return(apply(arr,3,Rhat1))
-        }
-        #############################################################################
-#	if (class( mice.object) == "mids.1chain"){	
-#		mice.object <- mice.object$midsobj		
-#		}
-    chainMean <- mice.object$chainMean
-    chainVar <- mice.object$chainVar
-    dcM <- dim(chainMean)
-    dfr <- data.frame(matrix( 0 , nrow= dcM[1] , ncol= 4 ))
-    for (vv in 1:dcM[1] ){ 
-        # vv <- 1
-        dfr[vv,3] <- Rhat( chainMean[vv,,] )
-        dfr[vv,4] <- Rhat( chainVar[vv,,] )
-        dfr[vv,1] <- rownames(chainMean[,,1])[vv]
-        dfr[vv,2] <- 100*mean( is.na( mice.object$data[ , dfr[vv,1] ] ) )
-                }
-    colnames(dfr) <- c("variable" , "MissProp" , "Rhat.M.imp" , "Rhat.Var.imp" )
-    print(dfr )
-    invisible(dfr)
-        }
+        }		
